17e96b635363ab8f2e02f077cd51ce10fd1668ad
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-24 13:10:50 +0200

    Stworzenie Klasy Infografika i TestRegEx, i sprawdzenie wyszukiwania wzorców reguralnych w klasie TestRegEx

---
+++ src/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -0,0 +1,14 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+public class Infografika {
+    public String tytul;
+    public String adresStrony;
+    public String adresGrafiki;
+    public String adresMiniaturki;
+    public String szerokosc;
+    public String wysokosc;
+
+    Infografika(String tekst){
+
+    }
+}
