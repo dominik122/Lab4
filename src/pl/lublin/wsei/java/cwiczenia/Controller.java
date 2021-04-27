e16194f17aff7ac484e93934f0da826cccbafeb0
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-27 11:09:01 +0200

    Dodanie komponentów Button i Label, obsługa zdarzenia wciśnięcia przycisku

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,4 +1,29 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+    public Label lbFile;
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML(*.xml)", "*.xml");
+
+    @FXML
+    public void initialize(){
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+
+        if(file!=null){
+            lbFile.setText(file.getAbsolutePath());
+        } else {
+            lbFile.setText("Proszę wczytać plik...");
+        }
+    }
 }
