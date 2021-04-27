4ad21b2bdb0bdf434fe0c8a05bf84c3fa52a3151
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-27 11:18:05 +0200

    Dodanie komponentu ListView i dodawanie do niego listy tytułów  w trakcie obsługi zdarzenia btnOpenFileAction

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,8 +1,11 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -11,6 +14,10 @@ public class Controller {
     public Label lbFile;
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML(*.xml)", "*.xml");
+
+    public ListView lstInfografiki;
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfographicList igList;
 
     @FXML
     public void initialize(){
@@ -21,7 +28,10 @@ public class Controller {
         File file = fileChooser.showOpenDialog(null);
 
         if(file!=null){
+            igList = new GusInfographicList(file.getAbsolutePath());
             lbFile.setText(file.getAbsolutePath());
+            for (Infografika ig: igList.infografiki) tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
         } else {
             lbFile.setText("Proszę wczytać plik...");
         }
